ef4c1f0c02a60ff772da8a1f88568559d8718b8a
EGOIST <0x142857@gmail.com>
2021-02-18 16:24:08 +0800

    fix: use named imports

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-import domToImage from "dom-to-image-chrome-fix-retina"
+import {
+  toPng as _toPng,
+  toJpeg as _toJpeg,
+  toBlob as _toBlob,
+} from "dom-to-image-chrome-fix-retina"
 import { changeDpiDataUrl, changeDpiBlob } from "changedpi"
 
 export interface Options {
@@ -15,19 +19,17 @@ export interface Options {
 const DPI = 72 * 2
 
 export const toPng = (el: Node, options?: Options): Promise<string> => {
-  return domToImage(el, options)
-    .toPng()
-    .then((dataUrl: string) => changeDpiDataUrl(dataUrl, DPI))
+  return _toPng(el, options).then((dataUrl: string) =>
+    changeDpiDataUrl(dataUrl, DPI)
+  )
 }
 
 export const toJpeg = (el: Node, options?: Options): Promise<string> => {
-  return domToImage(el, options)
-    .toJpeg()
-    .then((dataUrl: string) => changeDpiDataUrl(dataUrl, DPI))
+  return _toJpeg(el, options).then((dataUrl: string) =>
+    changeDpiDataUrl(dataUrl, DPI)
+  )
 }
 
 export const toBlob = (el: Node, options?: Options): Promise<Blob> => {
-  return domToImage(el, options)
-    .toBlob()
-    .then((blob: Blob) => changeDpiBlob(blob, DPI))
+  return _toBlob(el, options).then((blob: Blob) => changeDpiBlob(blob, DPI))
 }
